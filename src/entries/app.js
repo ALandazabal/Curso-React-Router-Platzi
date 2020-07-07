37f73df1fc6b3ef5a98ebdcf45129f4e1a4cd9b5
Angelica Landazabal <angelica.informatik@gmail.com>
2020-07-07 14:11:12 -0500

    Adding header

--- src/entries/app.js
+++ src/entries/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home';
 import { Provider } from 'react-redux';
@@ -9,6 +9,9 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { BrowserRouter } from 'react-router-dom';
+
+import Header from '../pages/components/header'
+
 // function logger({ getState, dispatch}) {
 //   return (next) => {
 //     return (action) => {
@@ -46,11 +49,12 @@ const homeContainer = document.getElementById('home-container')
 
 
 render(
-  <BrowserRouter
-    basename="/videos"
-  >
+  <BrowserRouter>
     <Provider store={store}>
-      <Home />
+      <Fragment>
+        <Home />
+        <Header />
+      </Fragment>
     </Provider>
   </BrowserRouter>
 , homeContainer);
